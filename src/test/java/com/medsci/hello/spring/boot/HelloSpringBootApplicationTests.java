59103b11fc8aeec74c98b498078ab546de6feac4
xuezhang <kang.zheng@bioon.com>
2020-06-28 15:56:18 +0800

    ++++

--- src/test/java/com/medsci/hello/spring/boot/HelloSpringBootApplicationTests.java
+++ src/test/java/com/medsci/hello/spring/boot/HelloSpringBootApplicationTests.java
@@ -44,4 +44,21 @@ class HelloSpringBootApplicationTests {
 
         }
     }
+
+    /**
+     * String 类型
+     */
+    @Test
+    public void str()
+    {
+        String s1 = new String("java");
+
+        String s2 = s1.intern();
+
+        String s3 = "java";
+
+        System.out.println(s2);
+        System.out.println(s1 == s2);
+        System.out.println(s3 == s2);
+    }
 }
